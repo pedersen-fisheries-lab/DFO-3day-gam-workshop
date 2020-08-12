0d6aa7909586544b7e0f5cf418cb36776b9060af
David Lawrence Miller <github@ninepointeightone.net>
2020-08-12 21:02:39 +0100

    solutions to 2

--- 02-extending-gams.R
+++ 02-extending-gams.R
@@ -31,26 +31,44 @@ b_shrimp_template <- gam(shrimp ~ s(x, y),
 
 ## solution
 
+# here I've fiddled with k to make it really big, see later slides/examples
+# to see how to do this!
+
 # negative binomial
-b_shrimp_nb <- gam(shrimp ~ s(x, y),
+b_shrimp_nb <- gam(shrimp ~ s(x, y, k=160),
                          data=shrimp2010,
                          family=nb(),
                          method="REML")
+
+# things to look at:
+# check that the "Family:" line is what you expect
+# check that the number in brackets (the negative binomial parameter) is estimated
 summary(b_shrimp_nb)
 
 # compare plot to gratia output
-draw(b_shrimp_nb) + coord_equal()
 plot(b_shrimp_nb, scheme=2, asp=1)
+# here coord_equal() does the same as asp=1 and sets the aspect ratio
+draw(b_shrimp_nb) + coord_equal()
+
 
 
 # tweedie
-b_shrimp_tw <- gam(shrimp ~ s(x, y),
+b_shrimp_tw <- gam(shrimp ~ s(x, y, k=160),
                          data=shrimp2010,
                          family=tw(),
                          method="REML")
+# things to look at:
+# check that the "Family:" line is what you expect
+# check that the number in brackets (the Tweedie power parameter) is estimated
 summary(b_shrimp_tw)
 
+# compare this to the above nb plot
+# not much difference (general shape is the same)
+# but the scale is different!
 draw(b_shrimp_tw) + coord_equal()
+
+
+
 # ---
 
 ## Exercise 2
@@ -65,8 +83,42 @@ draw(b_shrimp_tw) + coord_equal()
 # 2. look at the summary of this model and compare to those in the slides
 # 3. plot the results!
 
-mega_shrimp <- gam(shrimp ~ offset(log(area_trawled)) +
-                        __YOUR_FORMULA_HERE__,
-                data=shrimp2010,
-                family=__OTHER_DISTRIBUTIONS__,
-                method="REML")
+mega_shrimp <- gam(shrimp ~ __YOUR_FORMULA_HERE__,
+                   data=shrimp2010,
+                   family=__OTHER_DISTRIBUTIONS__,
+                   method="REML")
+
+## solution
+
+# I'll use Tweedie (tw()) for all these examples to keep things simple
+
+# building the model with te()
+mega_shrimp_te <- gam(shrimp ~ s(x, y, k=40) +
+                        # note here I prefer to specify 2 ks to remind myself that
+                        # this will lead to 25 basis functions!
+                               te(temp_bottom, depth, k=c(6, 6)),
+                      data=shrimp2010,
+                      family=tw(),
+                     method="REML")
+summary(mega_shrimp_te)
+
+# now with ti()
+mega_shrimp_ti <- gam(shrimp ~ s(x, y, k=40) +
+                               ti(temp_bottom, depth, k=c(6, 6)) +
+                               ti(temp_bottom, k=6) +
+                               ti(depth, k=6),
+                      data=shrimp2010,
+                      family=tw(),
+                      method="REML")
+summary(mega_shrimp_ti)
+
+# notes/things to think about
+#  - the sum of the EDFs for the ti() terms in mega_shrump_ti should be
+#    roughly the EDF for the te() term in mega_shrimp_te
+#  - in the ti() formulation we see that temp_bottom has EDF ~= 1 & isn't
+#    significant
+
+# gratia produces much nicer plots!
+draw(mega_shrimp_te)
+draw(mega_shrimp_ti)
+
